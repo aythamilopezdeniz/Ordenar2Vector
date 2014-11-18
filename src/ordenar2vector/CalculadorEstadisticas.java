e4f063800602bb67383fa51083b5e16d7e1ab060
Acer <Acer@Acer1>
2014-11-18 17:04:10 +0000

    Versión 1

--- src/ordenar2vector/CalculadorEstadisticas.java
+++ src/ordenar2vector/CalculadorEstadisticas.java
@@ -5,13 +5,20 @@ public class CalculadorEstadisticas {
             int tam_vector, boolean modo){
         DatosEstadisticos datosEstadisticos=new DatosEstadisticos();
         int[] vec=GeneraCaso.generaVector(tam_vector, modo);
-        CompruebaCaso.compruebaVector(vec, tam_vector);
         m.ordena(vec, datosEstadisticos);
+        CompruebaCaso.compruebaVector(vec, tam_vector);
         return datosEstadisticos;
     }
     
-    public static DatosEstadisticos estadisticaOrdenaVectorAleatorio(
+    public static MediaDatosEstadisticos estadisticaOrdenaVectorAleatorio(
             OrdenarVector m, int tam_vector){
+        int[] v=new int[10];
+        for(int i=0;i<v.length;i++){
+            DatosEstadisticos datosEstadisticos=new DatosEstadisticos();
+            v=GeneraCaso.generaVector(tam_vector, true);
+            m.ordena(v, datosEstadisticos);
+            CompruebaCaso.compruebaVector(v, tam_vector);
+        }
         return null;
     }
     
@@ -20,7 +27,7 @@ public class CalculadorEstadisticas {
         return null;
     }
     
-    public static DatosEstadisticos[] estadisticasOrdenaVectoresAleatorios(
+    public static MediaDatosEstadisticos[] estadisticasOrdenaVectoresAleatorios(
             OrdenarVector m, int[] tam_vectores){
         return null;
     }
--- src/ordenar2vector/ComparaMetodos.java
+++ src/ordenar2vector/ComparaMetodos.java
@@ -4,12 +4,13 @@ public class ComparaMetodos {
 
     public static void main(String[] args) {
         int[] v=new int[]{1000, 2000, 4000, 8000, 16000, 32000};
-        DatosEstadisticos vec1[],vec2[];
+        MediaDatosEstadisticos vec1[];
+        DatosEstadisticos vec2[];
         System.out.println("            Vector en orden inversoTiempo   Vector en orden aleatorioTiempo");
         vec1 = CalculadorEstadisticas.estadisticasOrdenaVectoresAleatorios(new Ordenar1Vector(), v);
         vec2 = CalculadorEstadisticas.estadisticasOrdenaVectoresInversos(new Ordenar1Vector(), v);
         for(int i=0;i<v.length;i++) {
             System.out.printf("%6d         %15f             %15f\n", 
-                    v[i], vec1[i].dameTiempo(), vec2[i].dameTiempo());
+                    v[i], vec1[i].dameMediaTiempos(), vec2[i].dameTiempo());
         }    }
 }
