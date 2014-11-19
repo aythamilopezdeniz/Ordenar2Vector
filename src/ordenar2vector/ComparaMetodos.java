1c54cbd3413ed7792d0e663d4adb42b578696bbf
usuario <usuario@10.22.143.41>
2014-11-19 12:22:08 +0000

    Versión 1

--- src/ordenar2vector/ComparaMetodos.java
+++ src/ordenar2vector/ComparaMetodos.java
@@ -13,26 +13,26 @@ public class ComparaMetodos {
         vec3 = CalculadorEstadisticas.estadisticasOrdenaVectoresAleatorios(new Ordenar2Vector(), v);
         vec4 = CalculadorEstadisticas.estadisticasOrdenaVectoresInversos(new Ordenar2Vector(), v);
         for(int i=0;i<v.length;i++) {
-            System.out.printf("%6d         %15f             %15f             %15f\n", 
+            System.out.printf("%6d         %15f       %15f     %15f\n", 
                     v[i], vec1[i].dameMediaTiempos(), vec1[i].dameMediaComparaciones(), 
                     vec1[i].dameMediaMovimientos());
         }
         System.out.println();
         for (int i=0;i<v.length;i++) {
-            System.out.printf("%6d         %15f       %15d       %15d\n", 
+            System.out.printf("%6d         %15f       %15d     %15d\n", 
                     v[i], vec2[i].dameTiempo(), vec2[i].dameComparaciones(), 
                     vec2[i].dameMovimientos());
         }
         System.out.println();
         System.out.println("                                    Ordenar2Vector");
         for (int i=0;i<v.length;i++) {
-            System.out.printf("%6d         %15f       %15f       %15f\n", 
+            System.out.printf("%6d         %15f       %15f     %15f\n", 
                     v[i], vec3[i].dameMediaTiempos(), vec3[i].dameMediaComparaciones(), 
                     vec3[i].dameMediaMovimientos());
         }
         System.out.println();
         for (int i=0;i<v.length;i++) {
-            System.out.printf("%6d         %15f       %15d       %15d\n", 
+            System.out.printf("%6d         %15f       %15d     %15d\n", 
                     v[i], vec4[i].dameTiempo(), vec4[i].dameComparaciones(), 
                     vec4[i].dameMovimientos());
         }
